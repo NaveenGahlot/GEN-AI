888c9e89320ad1e68615cd8ba8adc13abccd579a
NaveenGahlot <naveengahlotnkt@gmail.com>
2026-03-17 19:17:08 +0530

    fix it

--- backend/src/controllers/interview.controller.js
+++ backend/src/controllers/interview.controller.js
@@ -88,24 +88,35 @@ export const getAllInterviewReportController = async(req, res)=>{
  * @description Controller to generate resume PDF based on user self description, resume and job description.
  */
 export const generateResumePdfController = async(req, res) => {
-    const { interviewReportId } = req.params
+    try {
+        const { interviewReportId } = req.params
 
-    const interviewReport = await interviewReportModel.findById(interviewReportId)
+        const interviewReport = await interviewReportModel.findOne({
+            _id: interviewReportId,
+            user: req.user.id
+        })
 
-    if (!interviewReport) {
-        return res.status(404).json({
-            message: "Interview report not found."
+        if (!interviewReport) {
+            return res.status(404).json({
+                message: "Interview report not found."
+            })
+        }
+
+        const { resume, jobDescription, selfDescription } = interviewReport
+
+        const pdfBuffer = await generateResumePdf({ resume, jobDescription, selfDescription })
+
+        res.set({
+            "Content-Type": "application/pdf",
+            "Content-Disposition": `attachment; filename=resume_${interviewReportId}.pdf`
+        })
+
+        res.send(pdfBuffer)
+    } catch (err) {
+        console.error("Error generating resume PDF:", err)
+        res.status(500).json({
+            message: "Failed to generate resume PDF",
+            error: err.message
         })
     }
-
-    const { resume, jobDescription, selfDescription } = interviewReport
-
-    const pdfBuffer = await generateResumePdf({ resume, jobDescription, selfDescription })
-
-    res.set({
-        "Content-Type": "application/pdf",
-        "Content-Disposition": `attachment; filename=resume_${interviewReportId}.pdf`
-    })
-
-    res.send(pdfBuffer)
 }
