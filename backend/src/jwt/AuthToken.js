a9999ef492696a154253cfc1ecdf7d361dabb27e
NaveenGahlot <naveengahlotnkt@gmail.com>
2026-03-17 18:03:47 +0530

    fix it

--- backend/src/jwt/AuthToken.js
+++ backend/src/jwt/AuthToken.js
@@ -8,7 +8,7 @@ const createTokenAndSaveCookie = async(userId, res)=>{
     });
     res.cookie("jwt", token, {
         httpOnly: true,
-        secure: true,
+secure: process.env.NODE_ENV === 'production',
         sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000
     });
