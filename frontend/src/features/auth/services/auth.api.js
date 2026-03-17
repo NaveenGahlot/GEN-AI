0e03819e163a767cfe8bda71748ba4fea400c031
NaveenGahlot <naveengahlotnkt@gmail.com>
2026-03-17 15:23:29 +0530

    chore: commit pending changes

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://roleplay-ai-rob1.onrender.com/",
     withCredentials: true
 })
 
